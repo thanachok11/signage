7562883cfbfbde6f42cf8e5a4773c005be7a8b55
ธนาโชค สุวรรณ์ <thanachok.s@kkumail.com>
2026-01-29 12:57:41 +0700

    update

---  src/config.ts
+++  src/config.ts
@@ -1,4 +1,4 @@
-export const SIGNAGE_URL = "https://kiosk-pos.aicard.work/Queue?shopId=8445453f-23dd-4b39-baf0-124a01c86063&branchId=7f4bc846-28fa-460e-ab4a-dc449f69b05f";
+export const SIGNAGE_URL = "https://kiosk-pos.aicard.work/Queue?shopId=8445453f-23dd-4b39-baf0-124a01c86063&branchId=0f6ff938-1c40-4502-b234-85c7ef2b94d1";
 
 // รีโหลดกันค้างทุกกี่ ms (เช่น 5 นาที)
 export const HARD_RELOAD_INTERVAL_MS = 5 * 60 * 1000;
